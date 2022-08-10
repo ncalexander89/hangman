b6db2385d6289edc4cc0fb53bdf7b05551cf47a4
Chris <nc.alexander89@gmail.com>
2022-08-10 21:34:12 +1000

    added game win method

--- hangman.rb
+++ hangman.rb
@@ -1,19 +1,33 @@
 class Hangman
-  attr_reader :secret_word
+  attr_accessor :guessed_words, :guess, :secret_word
 
   def initialize
     @dictionary = File.readlines('dictionary.csv')
     @usable_words = []
+    @guessed_words = []
     @dictionary.each do |word|
       @usable_words.push(word) if word.length > 4 && word.length < 13
     end
-    puts @secret_word = @usable_words.sample
-    @empty = '_ '
-    puts "\n\n"
-    @secret_word.length.times {print @empty}
+    @secret_word = @usable_words.sample.chomp
+    @blank = @secret_word.split('')
+    @word = @blank.dup
+  end
+
+  def blank
+    puts "\n"
+    @blank = @word.map do |letter|
+      if !@guessed_words.include?(letter)
+        letter = '_ '
+      else "#{letter} "
+      end
+    end
+    @blank = @blank.join('')
+    print @blank
   end
 
   def player_guess
+    loop do
+    blank
     loop do
       puts "\n\nPick a letter or guess the word!"
       @guess = gets.chomp
@@ -25,12 +39,26 @@ class Hangman
         break
       end
     end
-  end
-
-  def guess_letter
-    puts @secret_word
+    return if check_win == true
   end
 end
+
+  def guess_letter
+    @guessed_words.push(@guess)
+    blank
+    puts "\n\n\n #{@secret_word}"
+  end
+
+  def check_win
+    if !@blank.include?('_')
+      puts 'you win'
+      return true
+    end
+  end
+
+end
+
+
 
 game = Hangman.new
 game.player_guess
